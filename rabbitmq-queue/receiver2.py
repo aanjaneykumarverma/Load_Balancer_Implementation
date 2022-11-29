59fe84059ffe9a77d2c15aef3d4a458ebe821f5d
kaushalkuma-r <2019ucs0077@iitjammu.ac.in>
2022-11-29 23:35:58 +0530

    Personalised callbacks for requests

--- rabbitmq-queue/receiver2.py
+++ rabbitmq-queue/receiver2.py
@@ -1,12 +1,23 @@
 import pika, os
+import socket
+from subprocess import call
 
-# Access the CLODUAMQP_URL environment variable and parse it (fallback to localhost)
 url = os.environ.get('amqps://yymrssjo:LlMVqA-WoqZPQjN7co2LeEQEkjmJyHOV@puffin.rmq2.cloudamqp.com/yymrssjo', 'amqp://guest:guest@localhost:5672/%2f')
+hostname = socket.gethostname()
+IPAddr = socket.gethostbyname(hostname)
 params = pika.URLParameters(url)
 connection = pika.BlockingConnection(params)
-channel = connection.channel() # start a channel
-channel.queue_declare(queue='hello') # Declare a queue
+channel = connection.channel() 
+channel.queue_declare(queue='QUEUENAME')
 def callback(ch, method, properties, body):
+  if body["ip"]==str(IPAddr):
+    request=body["request"]
+    match request:
+      case "create_vm":
+        rc = call("./vm_creation.sh")
+      case "run_task":
+        rc=call("./task_driver.sh")
+
   print(" [x] Received " + str(body))
 
 channel.basic_consume('hello',
