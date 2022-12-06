d331a74774480671edf00e69dea9d58b890e293f
Aanjaney Kumar Verma <akuver@Aanjaney.local>
2022-12-06 21:26:38 +0530

    Formatting changes

--- rabbitmq-queue/master/demo.py
+++ rabbitmq-queue/master/demo.py
@@ -1,10 +1,10 @@
-di={"name":"kaushal"}
-st1=str(di)
+import socket
+di = {"name": "kaushal"}
+st1 = str(di)
 
 print(st1)
-dict1=eval(st1)
+dict1 = eval(st1)
 print(dict1["name"])
-import socket
 hostname = socket.gethostname()
 IPAddr = socket.gethostbyname(hostname)
 print(IPAddr)
--- rabbitmq-queue/master/receiver.py
+++ rabbitmq-queue/master/receiver.py
@@ -1,30 +1,36 @@
-import pika, os
+import pika
+import os
 import socket
 from subprocess import call
-import json,ast
+import json
+import ast
 
-url = os.environ.get('amqps://yymrssjo:LlMVqA-WoqZPQjN7co2LeEQEkjmJyHOV@puffin.rmq2.cloudamqp.com/yymrssjo', 'amqp://guest:guest@localhost:5672/%2f')
+url = os.environ.get('amqps://yymrssjo:LlMVqA-WoqZPQjN7co2LeEQEkjmJyHOV@puffin.rmq2.cloudamqp.com/yymrssjo',
+                     'amqp://guest:guest@localhost:5672/%2f')
 hostname = socket.gethostname()
 IPAddr = socket.gethostbyname(hostname)
 params = pika.URLParameters(url)
 connection = pika.BlockingConnection(params)
-channel = connection.channel() 
+channel = connection.channel()
 channel.queue_declare(queue='hello')
+
+
 def callback(ch, method, properties, body):
-  body1=body.decode()
-  request = eval(body1)
+    body1 = body.decode()
+    request = eval(body1)
 
-  if request["IP"]==str(IPAddr):
-    request=request["type"]
-    vm_name=request["vm_name"]
-    os.environ['vm_info'] = vm_name
-    match request:
-      case "create_vm":
-        rc = call("./vm_creation.sh")
-      case "run_task":
-        rc=call("./task_driver.sh")
+    if request["IP"] == str(IPAddr):
+        request = request["type"]
+        vm_name = request["vm_name"]
+        os.environ['vm_info'] = vm_name
+        match request:
+            case "create_vm":
+                rc = call("./vm_creation.sh")
+            case "run_task":
+                rc = call("./task_driver.sh")
 
-  print(" [x] Received " + str(body))
+    print(" [x] Received " + str(body))
+
 
 channel.basic_consume('hello',
                       callback,
