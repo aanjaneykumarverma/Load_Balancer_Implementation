45a0208a736dc0756a3f9a6073212dfb7f948bc5
kaushalkuma-r <2019ucs0077@iitjammu.ac.in>
2022-12-11 19:09:54 +0530

    QueueNames Fix

--- rabbitmq-queue/slave/receiver.py
+++ rabbitmq-queue/slave/receiver.py
@@ -4,25 +4,30 @@ import socket
 from subprocess import call
 import json
 import ast
-
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# connect() for UDP doesn't send packets
+s.connect(('10.0.0.0', 0)) 
+IPAddr = s.getsockname()[0]
+print(IPAddr)
+s.close()
 url ="amqps://szvohhtx:88eua6-Vh6JHx1aEhzIStXoq-oil8GmW@puffin.rmq2.cloudamqp.com/szvohhtx"
-hostname = socket.gethostname()
-IPAddr = socket.gethostbyname(hostname)
+
 params = pika.URLParameters(url)
 connection = pika.BlockingConnection(params)
 channel = connection.channel()
-channel.queue_declare(queue='hello')
+queue_name=str(IPAddr)
+channel.queue_declare(queue=queue_name)
 
 
 def callback(ch, method, properties, body):
     body1 = body.decode()
     request = eval(body1)
-
-    if request["IP"] == str(IPAddr):
-        request = request["type"]
-        vm_name = request["vm_name"]
-        os.environ['vm_name'] = vm_name
-        match request:
+    print(type(request))
+    if request["IP"] == "192.168.122.1":
+        type1 = request["type"]
+        vm_name1 = request["vm_name"]
+        os.environ['vm_name'] = vm_name1
+        match type1:
             case "create_vm":
                 rc = call("./vm_creation.sh")
             case "run_task":
@@ -34,7 +39,7 @@ def callback(ch, method, properties, body):
     print(" [x] Received " + str(body))
 
 
-channel.basic_consume('hello',
+channel.basic_consume(queue_name,
                       callback,
                       auto_ack=True)
 channel.start_consuming()
