9da1f0f388d72bfad7b281b9bb2aefca575931b0
kaushalkuma-r <2019ucs0077@iitjammu.ac.in>
2022-12-11 19:22:57 +0530

    QueueNames Fix

--- rabbitmq-queue/master/sender.py
+++ rabbitmq-queue/master/sender.py
@@ -1,28 +1,41 @@
 import pika
 import os
 import socket
-hostname = socket.gethostname()
-IPAddr = socket.gethostbyname(hostname)
-
 # Access the CLODUAMQP_URL environment variable and parse it (fallback to localhost)
 url ="amqps://szvohhtx:88eua6-Vh6JHx1aEhzIStXoq-oil8GmW@puffin.rmq2.cloudamqp.com/szvohhtx"
+
+
                      
 params = pika.URLParameters(url)
 connection = pika.BlockingConnection(params)
-queue_names = ['hello', "hello2"]  # ip of hosts
-requests = []
-
-dict1 = {"IP": str(IPAddr), "type": "create_vm", "vm_name": "ka"}
-st1 = str(dict1)
-print(st1)
-# while True:
-for i in queue_names:
-    channel = connection.channel()
-    # start a channel
-    channel.queue_declare(queue=i)  # Declare a queue
-    channel.basic_publish(exchange='',
-                          routing_key=i,
-                          body=st1)
+with open("ip.txt") as f:
+    lines = f.readlines()
+queue_names = []
+for line in lines:
+    queue_names.append(str(line))
+ # ip of hosts
+with open("runCommands.txt") as f:
+    commands=f.readlines()
+requests=[]
+for command in commands:
+    task={}
+    command=command.split()
+    task["IP"]=str(command[0])
+    task["type"]=str(command[1])
+    task["vm_name"]=str(command[2])
+    requests.append(task)
+for dict1 in requests:   
+    st1 = str(dict1)
+    print(st1)
+    # while True:
+    for i in queue_names:
+        print(i)
+        channel = connection.channel()
+        # start a channel
+        channel.queue_declare(queue=i)  # Declare a queue
+        channel.basic_publish(exchange='',
+                            routing_key=i,
+                            body=st1)
 
 
 print(" [x] Sent 'Hello World!'")
