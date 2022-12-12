3b3c81ca91250de858f78bdaf39459669232b146
Aanjaney Kumar Verma <akuver@Aanjaney.local>
2022-12-12 08:51:33 +0530

    Minor Changes

--- Integration/clientNode/listener.js
+++ Integration/clientNode/listener.js
@@ -46,7 +46,11 @@ class Listener {
     // this function will do the following things:
     // 1. update task result to the result obtained from running the task on VM
     // 2. delete the VMs in the file from running VM list
-    if (fs.existsSync('taskPID.txt')) {
+    const reqs = await Requests.find({
+      reqtype: 'checkResults',
+      host: String(process.env.IP),
+    });
+    if (fs.existsSync('taskPID.txt') && reqs.length != 0) {
       shell.exec('python3 ./scripts/taskResult.py');
       var obj = JSON.parse(fs.readFileSync('results.json', 'utf8'));
       const taskResults = obj['taskRes'];
@@ -73,25 +77,31 @@ class Listener {
   }
   async updateUsage() {
     // this function will check cpu and memory usage periodically and update it
-    shell.exec('bash ./scripts/vmInfo.sh');
-    const host = await Host.findOne({ ip: process.env.IP });
-    if (fs.existsSync('host_info.json')) {
-      var obj = JSON.parse(fs.readFileSync('host_info.json', 'utf8'));
-      const vmList = obj['VMS'];
-      const hostInfo = obj['host_specs'];
-      await Host.findByIdAndUpdate(host._id, {
-        cpu: Number(hostInfo['Total Memory Usage']),
-        memory: Number(hostInfo['CPU Usage']),
-      });
-      for (let i = 0; i < vmList.length; ++i) {
-        const curVM = vmList[i];
-        const vm = await VM.findOneAndUpdate(
-          { host: host._id, name: curVM.name },
-          {
-            cpu: Number(curVM.vm_cpu),
-            memory: Number(curVM.vm_mem),
-          }
-        );
+    const reqs = await Requests.find({
+      reqtype: 'getInfo',
+      host: String(process.env.IP),
+    });
+    if (reqs.length != 0) {
+      shell.exec('bash ./scripts/vmInfo.sh');
+      const host = await Host.findOne({ ip: process.env.IP });
+      if (fs.existsSync('host_info.json')) {
+        var obj = JSON.parse(fs.readFileSync('host_info.json', 'utf8'));
+        const vmList = obj['VMS'];
+        const hostInfo = obj['host_specs'];
+        await Host.findByIdAndUpdate(host._id, {
+          cpu: Number(hostInfo['Total Memory Usage']),
+          memory: Number(hostInfo['CPU Usage']),
+        });
+        for (let i = 0; i < vmList.length; ++i) {
+          const curVM = vmList[i];
+          const vm = await VM.findOneAndUpdate(
+            { host: host._id, name: curVM.name },
+            {
+              cpu: Number(curVM.vm_cpu),
+              memory: Number(curVM.vm_mem),
+            }
+          );
+        }
       }
     }
     await delay(1000 * 1 * 60);
